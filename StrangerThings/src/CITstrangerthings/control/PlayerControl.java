bc90c61fab88f7ad79c070d4730890ba565a3ad6
Raye Ang <Raye Ang@10.38.166.184>
2016-10-21 10:21:36 -0600

    21 Oct - Rachel made changes to the PlayerControl and created a test for playerAttack

--- StrangerThings/src/CITstrangerthings/control/PlayerControl.java
+++ StrangerThings/src/CITstrangerthings/control/PlayerControl.java
@@ -10,5 +10,15 @@ package CITstrangerthings.control;
  * @author tibbit13
  */
 public class PlayerControl {
-    
+    public double playerAttack (int userSwing, int weaponStrength) {
+        if (userSwing < 1 || userSwing > 75) {
+            return -1;
+         }
+        if (weaponStrength < 1) {
+            return -1;
+        }
+
+        double totalForce = weaponStrength * userSwing /60;
+        return totalForce;
+    }
 }
