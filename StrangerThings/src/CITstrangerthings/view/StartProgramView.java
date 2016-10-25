e0c04f397ab1f1260101a342a21d3217c7b3580a
tibbit13 <tibbit13@10.38.166.44>
2016-10-25 09:11:00 -0600

    25 October 2016 -- syntax resolved

--- StrangerThings/src/CITstrangerthings/view/StartProgramView.java
+++ StrangerThings/src/CITstrangerthings/view/StartProgramView.java
@@ -1,14 +1,14 @@
-/+
- + To change this license header, choose License Headers in Project Properties.
- + To change this template file, choose Tools | Templates
- + and open the template in the editor.
- +/
+///+
+// + To change this license header, choose License Headers in Project Properties.
+// + To change this template file, choose Tools | Templates
+// + and open the template in the editor.
+// +/
 package CITstrangerthings.view;
 
-/++
- +
- + @author Raye Ang
- +/
+///++
+// +
+// + @author Raye Ang
+// +/
 public class StartProgramView {
 
     private String promptMessage;
