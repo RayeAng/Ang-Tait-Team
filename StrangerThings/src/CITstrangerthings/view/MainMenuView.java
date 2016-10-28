4505dfbac96e2d92fdb0720445b9f595a75e30e2
tibbit13 <tibbit13@10.38.166.44>
2016-10-28 10:11:49 -0600

    28 October 2016 -- main menu on its way to implementization! PART II

--- StrangerThings/src/CITstrangerthings/view/MainMenuView.java
+++ StrangerThings/src/CITstrangerthings/view/MainMenuView.java
@@ -5,6 +5,10 @@
  */
 package CITstrangerthings.view;
 
+import CITstrangerthings.control.GameControl;
+import java.util.Scanner;
+import strangerthings.StrangerThings;
+
 /**
  *
  * @author Raye Ang
@@ -41,13 +45,76 @@ public MainMenuView(){
     }
 
     private String getMenuOption() {
-        System.out.println("\n*** getMenuOption() function has been called***");
-        return "N";
+       
+        Scanner keyboard = new Scanner(System.in);
+        String value = "";
+        boolean valid = false;
+
+        while (!valid) {
+            System.out.println("\n" + this.menu);
+
+            value = keyboard.nextLine();
+            value = value.trim();
+
+            if (value.length() < 1) {
+                System.out.println("\nInvalid valude: value can not be blank");
+                continue;
+            }
+            break;
+        }
+        return value;
+
     }
 
     private boolean doAction(String menuOption) {
-        System.out.println("\n*** doAction() function has been called***");
-        return true;
+        menuOption = menuOption.toUpperCase();
+        
+        switch (menuOption){
+            case "N":
+                this.startNewGame();
+                break;
+            case "L":
+                this.loadExistingGame();
+                break;
+            case "S":
+                this.saveGame();
+                break;
+            case "Q": 
+                this.quitGame();
+                break;
+            case "H":
+                this.displayHelpMenu();
+                break;
+            default: 
+                System.out.println("\n*** Invalid Selection, try again. ***");
+                break;
+                   
+        }
+        return false;
+    }
+
+    private void startNewGame() {
+        
+        GameControl.createNewGame(StrangerThings.getPlayer());
+        
+        GameMenuView gameMenu = new GameMenuView();
+        gameMenu.displayMenu();
+    }
+
+    private void loadExistingGame() {
+        System.out.println("*** load game successful ***");
+    }
+
+    private void saveGame() {
+        System.out.println("*** save game successful ***");
+    }
+
+    private void quitGame() {
+        System.out.println("*** quit game successful ***");
+    }
+
+    private void displayHelpMenu() {
+        System.out.println("*** help me successful ***");
     }
     
 }
