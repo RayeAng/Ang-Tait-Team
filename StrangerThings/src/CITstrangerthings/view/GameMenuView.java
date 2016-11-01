ae28f0e1423f756f7fa72b19bf5132ef90170085
tibbit13 <tibbit13@10.38.166.44>
2016-11-01 09:52:42 -0600

    1 November, 2016 -- gameMenu skeleton created, still need to add specifics.

--- StrangerThings/src/CITstrangerthings/view/GameMenuView.java
+++ StrangerThings/src/CITstrangerthings/view/GameMenuView.java
@@ -5,14 +5,129 @@
  */
 package CITstrangerthings.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author tibbit13
  */
-class GameMenuView {
-    
-    void displayMenu(){
-        System.out.println("\n*** display menu function called ***");
+public class GameMenuView {
+
+    private String game;
+
+    public void displayMenu() {
+
+        boolean done = false;
+        do {
+            String gameOption = this.getGameOption();
+            if (gameOption.toUpperCase().equals("Q")) {
+                return;
+            }
+
+            done = this.doAction(gameOption);
+
+        } while (!done);
     }
-    
+
+    public GameMenuView() {
+        this.game = "\n"
+                + "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+                + "\n| Game Menu"
+                + "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+                + "\nM - Move"
+                + "\nI - Inventory"
+                + "\nV - View Map"
+                + "\nS - Save Game"
+                + "\nH - Help"
+                + "\nW - Weapon Manufacture/Status"
+                + "\nL - Look for Items"
+                + "\nQ - Exit Help Menu"
+                + "\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^";
+    }
+
+    private String getGameOption() {
+        Scanner keyboard = new Scanner(System.in);
+
+        String value = "";
+        boolean valid = false;
+
+        while (!valid) {
+            System.out.println("\n" + this.game);
+
+            value = keyboard.nextLine();
+            value = value.trim();
+
+            if (value.length() < 1) {
+                System.out.println("\nInvalid valude: value can not be blank");
+                continue;
+            }
+            break;
+        }
+        return value;
+
+    }
+
+    private boolean doAction(String gameOption) {
+        gameOption = gameOption.toUpperCase();
+
+        switch (gameOption) {
+            case "M":
+                this.showMove();
+                break;
+            case "I":
+                this.showInventory();
+                break;
+            case "V":
+                this.showMap();
+                break;
+            case "S":
+                this.saveGame();
+                break;
+            case "H":
+                this.showHelp();
+                break;
+            case "W":
+                this.showWeapon();
+                break;
+            case "L":
+                this.lookForItems();
+                break;
+
+            default:
+                System.out.println("\n*** Invalid Selection, try again. ***");
+                break;
+
+        }
+        return false;
+    }
+
+    private void showMove() {
+        System.out.println("\n***show move working   ***");
+    }
+
+    private void showInventory() {
+        System.out.println("\n*** show inventory working   ***");
+    }
+
+    private void showMap() {
+        System.out.println("\n*** show map working  ***");
+    }
+
+    private void saveGame() {
+        System.out.println("\n*** save game working  ***");
+    }
+
+    private void showHelp() {
+        HelpView helpView = new HelpView();
+        helpView.displayHelpView();
+    }
+
+    private void showWeapon() {
+        System.out.println("\n*** show weapon working  ***");
+    }
+
+    private void lookForItems() {
+        System.out.println("\n***  looking for items, ahoy!   ***");
+    }
+
 }
