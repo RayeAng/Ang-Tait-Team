6f1b5baf6b90321aa850758f6c538fabe0e2947b
Raye Ang <Raye Ang@10.38.166.184>
2016-11-09 09:51:40 -0700

    9 Nov - Rachel made changes to the MoveView menu

--- StrangerThings/src/CITstrangerthings/view/MoveView.java
+++ StrangerThings/src/CITstrangerthings/view/MoveView.java
@@ -11,55 +11,19 @@ import java.util.Scanner;
  *
  * @author Raye Ang
  */
-public class MoveView {
-
-    private String moveOption;
-
-    public void displayMoveView() {
-        boolean done = false;
-        do {
-            String moveOption = this.getMoveOption();
-
-            if (moveOption.toUpperCase().equals("Q")) {
-                return;
-            }
-            done = this.doAction(moveOption);
-        } while (!done);
-    }
+public class MoveView extends View {
 
     public MoveView() {
-        this.moveOption
-                = "\n"
+        super("\n"
                 + "\nN - Move North"
                 + "\nS - Move South"
                 + "\nW - Move West"
                 + "\nE - Move East"
-                + "\nQ - Exit move menu";
+                + "\nQ - Exit move menu");
     }
-
-    private String getMoveOption() {
-
-        Scanner keyboard = new Scanner(System.in);
-        String value = "";
-        boolean valid = false;
-
-        while (!valid) {
-            System.out.println("\n" + this.moveOption);
-
-            value = keyboard.nextLine();
-            value = value.trim();
-
-            if (value.length() > 1) {
-                System.out.println("\nInvalid answer: Pick a letter to move.");
-                continue;
-            }
-            break;
-        }
-        return value;
-
-    }
-
-    private boolean doAction(String moveOption) {
+    
+    @Override
+    public boolean doAction(String moveOption) {
         moveOption = moveOption.toUpperCase();
 
         switch (moveOption) {
