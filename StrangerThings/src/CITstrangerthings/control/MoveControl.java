b20a0741d95e75b5d0cac1630a1389a2d32be894
Raye Ang <Raye Ang@10.38.166.184>
2016-12-08 11:13:28 -0700

    8 Dec - Working on the MoveControl

--- StrangerThings/src/CITstrangerthings/control/MoveControl.java
+++ StrangerThings/src/CITstrangerthings/control/MoveControl.java
@@ -5,10 +5,33 @@
  */
 package CITstrangerthings.control;
 
+import CITstrangerthings.model.Location;
+import CITstrangerthings.model.Character;
 /**
  *
  * @author Raye Ang
  */
 public class MoveControl {
-    
+    public void move (CITstrangerthings.model.Character character, Location[][] locations, String direction) {
+        if (direction == "S") {
+            Location oldlocation = locations[character.getCoordinates().x][character.getCoordinates().y];
+            Location newLocation = locations[character.getCoordinates().x + 1][character.getCoordinates().y];
+        }
+        else if (direction == "N") {
+            Location oldlocation = locations[character.getCoordinates().x][character.getCoordinates().y];
+            Location newLocation = locations[character.getCoordinates().x - 1][character.getCoordinates().y];
+        }
+        else if (direction == "E") {
+            Location oldlocation = locations[character.getCoordinates().x][character.getCoordinates().y];
+            Location newLocation = locations[character.getCoordinates().x][character.getCoordinates().y + 1];
+        }
+        else if (direction == "W") {
+            Location oldlocation = locations[character.getCoordinates().x][character.getCoordinates().y];
+            Location newLocation = locations[character.getCoordinates().x][character.getCoordinates().y - 1];
+        }
+        else {
+            System.out.println("Where did you want to go??");
+        }
+        
+    }
 }
